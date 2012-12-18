bb408965dc04eb6fb331301909d3aa8b2f856281
ALJI Mohamed <mr.m.alji@gmail.com>
2012-12-18 22:21:32 +0000

    Replaced the Beta with Log4J v1.2
    removed Log4J 2.0 beta 3
    added Log4J 1.2.17
    updated ignored files list
    written HelloWorld in the main

--- Log4J-Newbie/src/aljilogy/log4jnewbie/main/HelloWorld.java
+++ Log4J-Newbie/src/aljilogy/log4jnewbie/main/HelloWorld.java
@@ -3,17 +3,31 @@
  */
 package aljilogy.log4jnewbie.main;
 
-/**
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
+/*
  * @author Mohamed ALJI
  *
  */
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
+	private static Logger logger = LogManager.getLogger("MyFirstLogger");
+	 
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		logger.info("my text to be logged as a INFO. ");
+		logger.debug("my text to be logged as a DEBUG");
+		logger.warn("my text to be logged as a WARN");
+		
+		SimpleLayout layout = new SimpleLayout();
+		ConsoleAppender stdout = new ConsoleAppender(layout);
+		logger.addAppender(stdout);
+
+
 
 	}
 
