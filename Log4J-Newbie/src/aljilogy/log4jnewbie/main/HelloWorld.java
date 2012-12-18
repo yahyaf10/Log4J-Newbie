40dd7f439bf841344c587f6184bda0a8c3cc6a77
ALJI Mohamed <mr.m.alji@gmail.com>
2012-12-18 20:33:25 +0000

    first commit : creation Java Project + linking to GitHub Project

---
+++ Log4J-Newbie/src/aljilogy/log4jnewbie/main/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package aljilogy.log4jnewbie.main;
+
+/**
+ * @author Mohamed ALJI
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
