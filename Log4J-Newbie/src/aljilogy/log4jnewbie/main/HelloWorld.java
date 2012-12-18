2f0c9c5f9b5b704e349accc7d5b6464ce500f6b2
ALJI Mohamed <mr.m.alji@gmail.com>
2012-12-18 22:38:10 +0000

    a running basic first example

--- Log4J-Newbie/src/aljilogy/log4jnewbie/main/HelloWorld.java
+++ Log4J-Newbie/src/aljilogy/log4jnewbie/main/HelloWorld.java
@@ -3,6 +3,7 @@
  */
 package aljilogy.log4jnewbie.main;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -18,14 +19,20 @@ public class HelloWorld {
 	 
 	 
 	public static void main(String[] args) {
+			
+		
+	    BasicConfigurator.configure();
+
+		//SimpleLayout layout = new SimpleLayout();
+		
+		//ConsoleAppender stdout = new ConsoleAppender(layout);
+		//logger.addAppender(stdout);
+		
+		// app running
 		
 		logger.info("my text to be logged as a INFO. ");
 		logger.debug("my text to be logged as a DEBUG");
 		logger.warn("my text to be logged as a WARN");
-		
-		SimpleLayout layout = new SimpleLayout();
-		ConsoleAppender stdout = new ConsoleAppender(layout);
-		logger.addAppender(stdout);
 
 
 
